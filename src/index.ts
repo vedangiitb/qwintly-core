6403180563ae832f9e4bd24044558d0366456490
vedangiitb <vedangbaleiitb@gmail.com>
2026-05-01 14:27:43 +0530

    adding workspacedeps

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export * from "./ai/tools/toolsets/plannerTools.js";
 export * from "./ai/tools/toolsets/codegenTools.js";
 
 export * from "./ai/tools/implementations/factories.js";
+export * from "./ai/tools/implementations/workspaceDeps.js"
 
 export * from "./utils/workspace.js";
 export * from "./utils/utils.js";
